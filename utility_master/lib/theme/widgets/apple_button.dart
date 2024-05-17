116e32914890e06b9775e82f954229dac7038c4b
Brian Neumann-Fopiano <mypsn1550@gmail.com>
2024-05-16 22:08:13 -0400

    butt

--- utility_master/lib/theme/widgets/apple_button.dart
+++ utility_master/lib/theme/widgets/apple_button.dart
@@ -1,33 +1,50 @@
+import 'package:common_svgs/common_svgs.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:padded/padded.dart';
-import 'package:utility_master/theme/widgets/text.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:utility_master/util/bloc/login.dart';
 
 class AppleSignInButton extends StatelessWidget {
   const AppleSignInButton({super.key});
 
   @override
-  Widget build(BuildContext context) => InkWell(
-        onTap: () => context.read<LoginBloc>().add(LoginAppleEvent()),
-        child: PaddingAll(
-          padding: 14,
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              PaddingRight(
-                padding: 14,
-                child: SvgPicture.string('''
-<svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" width="814" height="1000"><path d="M788 341c-6 4-108 62-108 190 0 149 130 201 134 203-1 3-21 71-69 141-42 62-87 124-155 124s-86-40-164-40c-77 0-104 41-166 41s-106-57-156-127A612 612 0 0 1 0 542c0-195 126-298 251-298 66 0 121 44 163 44 39 0 101-46 176-46 28 0 131 2 198 99zM554 159c31-36 53-88 53-139 0-7 0-14-2-20-50 2-110 34-147 76-28 32-55 83-55 135l2 18 14 2c45 0 102-31 135-72z"/></svg>
-''', width: 32, height: 32, color: Colors.white),
-              ),
-              UMT.txt(
-                context,
-                "Sign in with Apple",
-              )
-            ],
-          ),
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => context.read<LoginBloc>().add(LoginAppleEvent()),
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+        decoration: BoxDecoration(
+          color: Colors.black, // Apple's typical button color
+          borderRadius: BorderRadius.circular(4),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.25),
+              spreadRadius: 0,
+              blurRadius: 4,
+              offset: const Offset(0, 2), // Subtle shadow for elevation
+            ),
+          ],
         ),
-      );
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.string(
+              svgApple, // Assuming svgApple is defined elsewhere in your code
+              width: 32,
+              height: 32,
+            ),
+            const SizedBox(width: 10),
+            const Text(
+              'Sign in with Apple',
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
