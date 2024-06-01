2af33a12c8172acab704b579eff9052675b3767f
Brian Fopiano <29320316+NextdoorPsycho@users.noreply.github.com>
2024-05-31 20:28:42 -0400

    Add glassmorphic UI elements to split pages

---
+++ utility_master/lib/theme/glass_widget_design.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class GlassWidgetDesign {
+  static Widget GlassBox({
+    required double blurX,
+    required double blurY,
+    required Widget child,
+  }) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10.0),
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: blurX, sigmaY: blurY),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white.withOpacity(0.2),
+            borderRadius: BorderRadius.circular(10.0),
+            border: Border.all(
+              color: Colors.white.withOpacity(0.2),
+            ),
+          ),
+          child: child,
+        ),
+      ),
+    );
+  }
+}
