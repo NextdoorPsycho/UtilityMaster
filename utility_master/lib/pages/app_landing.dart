ffa2d781f955fadc0840dfa77cbd9d0ff36cf604
Brian Neumann-Fopiano <mypsn1550@gmail.com>
2024-07-17 23:37:37 -0400

    d

--- utility_master/lib/pages/app_landing.dart
+++ utility_master/lib/pages/app_landing.dart
@@ -2,9 +2,6 @@ import 'package:arcane/arcane.dart';
 import 'package:utility_master/pages/example_page.dart';
 import 'package:utility_master/pages/home.dart';
 import 'package:utility_master/pages/settings.dart';
-import 'package:utility_master/pages/showcase.dart';
-import 'package:utility_master/pages/split_horizontal_page.dart';
-import 'package:utility_master/pages/split_vertical_page.dart';
 import 'package:utility_master/theme/animated_sidebar.dart';
 
 class LandingPage extends ArcaneStatefulScreen {
@@ -23,11 +20,8 @@ class _LandingPageState extends State<LandingPage> {
   // Pages for navigation
   final List<Widget> _pages = [
     const Home(),
-    const ShowcasePage(),
     const LicensePage(),
     const ExamplePage(),
-    const SplitHorizontalPage(),
-    const SplitVerticalPage(),
     const SettingsPage(),
   ];
 
@@ -51,14 +45,7 @@ class _LandingPageState extends State<LandingPage> {
             sidebarItems: [
               SideBarItem(iconSelected: Icons.home, text: 'Home'),
               SideBarItem(iconSelected: Icons.build, text: 'Showcase'),
-              SideBarItem(iconSelected: Icons.info, text: 'License'),
               SideBarItem(iconSelected: Icons.code, text: 'Example'),
-              SideBarItem(
-                  iconSelected: Icons.vertical_split_outlined,
-                  text: 'Split Horizontal'),
-              SideBarItem(
-                  iconSelected: Icons.horizontal_split_outlined,
-                  text: 'Split Vertical'),
               SideBarItem(iconSelected: Icons.settings, text: 'Settings'),
             ],
             widthSwitch: 900, // Example breakpoint for a responsive layout
